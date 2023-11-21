6ebbaf008570077fbe214702c27f0d7df70d2e1d
Hadassah Uche <uhadassah1@gmail.com>
2023-11-21 22:38:01 +0200

    Add project files.

---
+++ UcheHadassah7_4/HadassahUche7_4.cpp
@@ -0,0 +1,6 @@
+/*a) Define a single structure data type suitable for a Car structure to hold the following 
+information: Car number, miles driven and gallons used.
+b) Using the data type declared above, write program that interactively accepts these 
+data into an array of 5 structures. Once the data have been entered, the program 
+should create a report listing each car number and the miles per gallon achieved by 
+the car.*/
