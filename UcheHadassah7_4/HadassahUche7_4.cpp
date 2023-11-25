dec2c7bcb2f632024525164416e9cb6dcdd7317f
Hadassah Uche <uhadassah1@gmail.com>
2023-11-25 23:31:55 +0200

    It keeps saying cout is ambiguous so I.m just gonna wipe it all and start afresh

--- UcheHadassah7_4/HadassahUche7_4.cpp
+++ UcheHadassah7_4/HadassahUche7_4.cpp
@@ -15,54 +15,5 @@ struct Car
 };
 int main()
 {
-	const int MAX_CARS = 5;
-	Car carData[MAX_CARS];
-	int i;
-
-	for (i = 0; i < MAX_CARS; i++)
-	{
-		cout << "\nEnter the details for Car " << i + 1;
-		cout << "\nCar Number:";
-		//Check for duplicate car numbers
-		for(int j = 0;j < i;j++)
-		{
-			cin.getline(carData[i].carNumber, 20);
-			while (strcmp(carData[i].carNumber,carData[j].carNumber) == 0)
-			{
-				cout << "Already exists. Enter a new car number:";
-				cin.getline(carData[i].carNumber, 20);
-				j = 0;//start checking from the top again
-				}
-			}
-		}
-		cout << "Enter the miles driven:";
-		do
-		{
-			cin >> carData[i].milesDriven;
-			while (carData[i].milesDriven < 0.0)
-			{
-				cout << "Incorrect data. Please enter the miles driven by car " << i + 1 << ":";
-				cin >> carData[i].milesDriven;
-			}
-		} while (carData[i].milesDriven < 0.0);
-		cout << "Enter the gallons used:";
-		do
-		{
-			cin >> carData[i].gallonsUsed;
-			while (carData[i].gallonsUsed < 0)
-			{
-				cout << "Incorrect data. Please enter the gallons used by car " << i + 1 << ":";
-				cin >> carData[i].gallonsUsed;
-			}
-		} while (carData[i].gallonsUsed < 0);
-		cin.ignore();
-	}
-	
-	cout << "Cars Report";
-	cout << "\nCar Number\tMiles/Gallon";
-	for (int j = 0; j < MAX_CARS; j++)
-	{
-		double milesPerGallon = carData[j].milesDriven / carData[j].gallonsUsed;
-		cout <<"\n"<< carData[j].carNumber<<"\t\t"<< milesPerGallon;
-	}
+	cout << "Hello World";
 }
